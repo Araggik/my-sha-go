3b77572e35ae32c138b5665c9b3dfbcd2b61a4a2
Vlad Matveev <vladislaw0159@gmail.com>
2025-07-22 11:55:33 +0500

    Сделал urlfetch

--- urlfetch/main.go
+++ urlfetch/main.go
@@ -2,6 +2,29 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+func check(e error) {
+	if e != nil {
+		os.Exit(1)
+	}
+}
+
+func main() {
+	urls := os.Args[1:]
+
+	for _, v := range urls {
+		resp, err := http.Get(v)
+		check(err)
+		body, err := io.ReadAll(resp.Body)
+		check(err)
+		fmt.Println(string(body))
+
+		resp.Body.Close()
+	}
 }
