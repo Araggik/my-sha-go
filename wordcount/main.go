a727df67666a5b4a293c95edf26b219b5edc77d0
Vlad Matveev <vladislaw0159@gmail.com>
2025-07-22 11:26:26 +0500

    Сделал wordcount

--- wordcount/main.go
+++ wordcount/main.go
@@ -2,6 +2,41 @@
 
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	fileNames := os.Args[1:]
+
+	m := make(map[string]int)
+
+	for _, fileName := range fileNames {
+		file, err := os.Open(fileName)
+		check(err)
+
+		scanner := bufio.NewScanner(file)
+
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			m[line]++
+		}
+
+		file.Close()
+	}
+
+	for k, v := range m {
+		if v > 1 {
+			fmt.Printf("%v\t%v\n", v, k)
+		}
+	}
 }
