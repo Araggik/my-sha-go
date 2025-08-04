906ece74861183d8f81c2ed283b17df212c5c260
Матвеев Владислав Анатольевич <matveev.va@corp.ertelecom.loc>
2025-08-04 08:55:26 +0500

    Проверить решение задачи utf8/reverse

--- utf8/reverse/reverse.go
+++ utf8/reverse/reverse.go
@@ -2,6 +2,40 @@
 
 package reverse
 
+import (
+	"strings"
+	"unicode/utf8"
+)
+
 func Reverse(input string) string {
-	return ""
+	var b strings.Builder
+
+	replacementChar := '\uFFFD'
+
+	for i := len(input) - 1; i >= 0; {
+		r := replacementChar
+
+		j := i + 1
+
+		//Ищем руну среди 1-4 байтов
+		for k := i; r == replacementChar && j-k <= 4; k-- {
+			r, _ = utf8.DecodeRuneInString(input[k:j])
+
+			if r != replacementChar {
+				b.WriteRune(r)
+				i = k - 1
+			}
+		}
+
+		//Если не нашли руну среди 4 байтов, то пишем 4 replacementChar
+		if r == replacementChar {
+			for k := 0; k < 4; k++ {
+				b.WriteRune(replacementChar)
+			}
+
+			i -= 4
+		}
+	}
+
+	return b.String()
 }
