0c49502aabc638fb688b09f4dac54ff42ba6cafe
Матвеев Владислав Анатольевич <matveev.va@corp.ertelecom.loc>
2025-08-06 08:32:23 +0500

    Проверить решение задачи varfmt

--- varfmt/fmt.go
+++ varfmt/fmt.go
@@ -2,6 +2,52 @@
 
 package varfmt
 
+import (
+	"fmt"
+	"strings"
+	"strconv"
+)
+
 func Sprintf(format string, args ...interface{}) string {
-	return ""
+	var b strings.Builder
+
+	braceChar1 := '{'
+	braceChar2 := '}'
+
+	isArgNumber := false
+
+	var argNumberStr strings.Builder
+
+	braceCount := -1
+
+	for _, r := range format {
+		switch  {
+		  case r == braceChar1:
+			isArgNumber = true
+
+			braceCount++
+		  case r == braceChar2 && isArgNumber:
+			isArgNumber = false
+
+			if argNumberStr.Len() == 0 {
+				b.WriteString(fmt.Sprint(args[braceCount]))
+			} else {
+				argNumber, err := strconv.Atoi(argNumberStr.String())
+
+				if err != nil {
+					panic("Некорректный индекс аргумента")
+				}
+	
+				b.WriteString(fmt.Sprint(args[argNumber]))
+	
+				argNumberStr.Reset()
+			}
+		  case isArgNumber:
+			argNumberStr.WriteRune(r)
+		  default:
+			b.WriteRune(r)
+		}
+	}
+
+	return b.String()
 }
