c8f567a3e3ede44ce8166345ad2cae6b8ac00366
Vlad Matveev <vladislaw0159@gmail.com>
2025-08-19 20:30:23 +0500

    Исправить otp

--- otp/cipher.go
+++ otp/cipher.go
@@ -7,62 +7,83 @@ import (
 )
 
 type CipherReader struct {
-	r io.Reader
+	r    io.Reader
 	prng io.Reader
 }
 
 func (cr *CipherReader) Read(p []byte) (n int, e error) {
 	rP := make([]byte, len(p))
 
-	//Читаем из r
-	if rN, rE := cr.r.Read(rp); rE == nil {
-		prngP := make([]byte, len(rP))
+	_, rE := cr.r.Read(rP)
 
-		//Если расшифровали меньше, то дорасшифровываем
-		for prngN :=0; prngN < rN; {
-			nextN, _ := cr.prng.Read(prngP)
+	if rE == nil {
+		prngN, prngE := cr.prng.Read(rP)
 
-			//Проверить, что [] вызываются раньше ...
-			rp = append(rp, ...prngP[:nextN])
+		if prngE == nil {
+			n = prngN
 
-			prngN += nextN;
+			p = p[:0]
+
+			_ = append(p, rP[:prngN]...)
+
+			//fmt.Println(rN, prngN)
+		} else {
+			e = prngE
 		}
 
-		//Возвращение n
-		n = rN
-			
-		p = p[:0]
-
-		p = append(p, ...rP)
 	} else {
 		e = rE
 	}
 
-	return;
+	// //Читаем из r
+	// if rN, rE := cr.r.Read(rP); rE == nil {
+	// 	prngP := make([]byte, len(rP))
+
+	// 	//Если расшифровали меньше, то дорасшифровываем
+	// 	for prngN := 0; prngN < rN; {
+	// 		nextN, _ := cr.prng.Read(prngP)
+
+	// 		//Проверить, что [] вызываются раньше ...
+	// 		rP = append(rP, prngP[:nextN]...)
+
+	// 		prngN += nextN
+	// 	}
+
+	// 	//Возвращение n
+	// 	n = rN
+
+	// 	p = p[:0]
+
+	// 	p = append(p, rP...)
+	// } else {
+	// 	e = rE
+	// }
+
+	return
 }
 
 type CipherWriter struct {
-	w io.Writer
+	w    io.Writer
 	prng io.Reader
 }
 
 func (cw *CipherWriter) Write(p []byte) (n int, e error) {
 	var copyP []byte
 
-	copyP = append(copyP, ...p)
+	copyP = append(copyP, p...)
 
 	l := len(copyP)
 
 	//Шифруем в copyP
-	for i := 0;  i < l; {
-		nextN, _  = cw.prng.Read(copyP[i:len])
+	for i := 0; i < l; {
+		nextN, _ := cw.prng.Read(copyP[i:l])
 
 		i += nextN
 	}
 
 	//Пишем в w
 	for i := 0; i < l && e == nil; {
-		nextN, err  = cw.w.Write(copyP[i:len])
+		nextN, err := cw.w.Write(copyP[i:l])
 
 		if err == nil {
 			i += nextN
@@ -76,7 +97,7 @@ func (cw *CipherWriter) Write(p []byte) (n int, e error) {
 		n = l
 	}
 
-	return 
+	return
 }
 
 func NewReader(r io.Reader, prng io.Reader) io.Reader {
