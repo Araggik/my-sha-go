db9493f2b4ddd5b4ba6df3382ebbb7445a823217
Vlad Matveev <vladislaw0159@gmail.com>
2025-08-23 11:00:00 +0500

    Доделать externalsort

--- externalsort/sort.go
+++ externalsort/sort.go
@@ -3,20 +3,22 @@
 package externalsort
 
 import (
+	"container/heap"
 	"io"
+	"os"
 	"strings"
 )
 
-const newLineByte = []byte("\n")[0]
+var newLineByte = []byte("\n")[0]
 
-//LineReader
+// LineReader
 type BaseLineReader struct {
-	r io.Reader
-	buf []byte
+	r      io.Reader
+	buf    []byte
 	bufLen int
 }
 
-func (b BaseLineReader) ReadLine() (string, error){
+func (b *BaseLineReader) ReadLine() (string, error) {
 	var builder strings.Builder
 
 	var s string
@@ -29,7 +31,7 @@ func (b BaseLineReader) ReadLine() (string, error){
 
 	//Нужно в builder записать то, что есть в буфере
 	//В буфере может быть несколько строк
-	if (l > 0) {
+	if l > 0 {
 		i := 0
 
 		for ; i < l && !hasStr; i++ {
@@ -68,11 +70,10 @@ func (b BaseLineReader) ReadLine() (string, error){
 				if hasStr {
 					b.buf = buf[j:]
 				}
-			} else  {
+			} else {
 				err = e
 			}
 		}
-
 
 	}
 
@@ -83,14 +84,13 @@ func (b BaseLineReader) ReadLine() (string, error){
 	return s, err
 }
 
-
-//LineWriter
+// LineWriter
 type BaseLineWriter struct {
 	w io.Writer
 }
 
 func (b BaseLineWriter) Write(l string) error {
-	var builder strings.builder
+	var builder strings.Builder
 
 	builder.WriteString(l)
 	builder.WriteByte(newLineByte)
@@ -103,26 +103,26 @@ func (b BaseLineWriter) Write(l string) error {
 	return err
 }
 
-//Merge
+// Merge
 type (
 	StrHeapElem struct {
 		readerIndex int
-		str string
+		str         string
 	}
 
-	StrHeap StrHeapElem[]
+	StrHeap []StrHeapElem
 )
 
 func (h *StrHeap) Len() int {
-	return len(StrHeap)
+	return len(*h)
 }
 
 func (h *StrHeap) Less(i, j int) bool {
-	return h[i].str < h[j].str
+	return (*h)[i].str < (*h)[j].str
 }
 
-func (h *StrHeap) Swap(i, j int) { 
-	h[i], h[j] = h[j], h[i] 
+func (h *StrHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
 func (h *StrHeap) Push(x any) {
@@ -137,20 +137,121 @@ func (h *StrHeap) Pop() any {
 	return x
 }
 
+// Sort
+type FileLines struct {
+	lines []string
+	index int
+}
+
+func (fl *FileLines) ReadFile(lr LineReader) {
+	for line, e := lr.ReadLine(); len(line) > 0 && e == nil; line, e = lr.ReadLine() {
+		fl.lines = append(fl.lines, line)
+	}
+}
+
+func (fl *FileLines) ReadLine() (string, error) {
+	var s string
+	var e error
+
+	if fl.index > len(fl.lines) {
+		e = io.EOF
+	} else {
+		s = fl.lines[fl.index]
+		fl.index++
+	}
+
+	return s, e
+}
+
 func NewReader(r io.Reader) LineReader {
 	const bufLen = 32
 
-	return BaseLineReader{r: r, buf: nil, bufLen: bufLen}
+	return &BaseLineReader{r: r, buf: nil, bufLen: bufLen}
 }
 
 func NewWriter(w io.Writer) LineWriter {
-	return BaseLineWriter(w: w)
+	return BaseLineWriter{w: w}
+}
+
+func sortFile(fileName string) error {
+	var err error
+
+	f, e := os.Open(fileName)
+
+	if e != nil {
+		return e
+	}
+
+	fl := &FileLines{lines: nil, index: 0}
+
+	r := NewReader(f)
+
+	fl.ReadFile(r)
+
+	f.Close()
+
+	writeFile, e := os.OpenFile(f.Name(), os.O_TRUNC, os.ModePerm)
+
+	if e == nil {
+		defer writeFile.Close()
+
+		for s, e := fl.ReadLine(); e == nil && err == nil; s, e = fl.ReadLine() {
+			_, err = writeFile.WriteString(s)
+		}
+	} else {
+		err = e
+	}
+
+	return err
 }
 
 func Merge(w LineWriter, readers ...LineReader) error {
-	
+	var err error
+
+	h := &StrHeap{}
+	heap.Init(h)
+
+	for i, v := range readers {
+		if s, e := v.ReadLine(); e == nil {
+			heap.Push(h, StrHeapElem{readerIndex: i, str: s})
+		}
+
+	}
+
+	for h.Len() > 0 && err == nil {
+		heapElem := heap.Pop(h).(StrHeapElem)
+
+		e := w.Write(heapElem.str)
+
+		if e == nil {
+			index := heapElem.readerIndex
+
+			s, e := readers[index].ReadLine()
+
+			if e == nil {
+				heap.Push(h, StrHeapElem{readerIndex: index, str: s})
+			}
+		} else {
+			err = e
+		}
+	}
+
+	return err
 }
 
 func Sort(w io.Writer, in ...string) error {
-	panic("implement me")
+	var err error
+
+	filesCount := len(in)
+
+	//Сортируем строки внутри файлов
+	for i := 0; i < filesCount && err == nil; i++ {
+		e := sortFile(in[i])
+
+		if e != nil {
+			err = e
+		}
+	}
+
+	return err
 }
