813e0cfa7fbcfead8669d7536fcb9afa98cd0e47
Vlad Matveev <vladislaw0159@gmail.com>
2025-08-27 12:27:33 +0500

    Сделал lrucache

--- lrucache/lru.go
+++ lrucache/lru.go
@@ -6,33 +6,33 @@ import "container/list"
 
 type LRUCache struct {
 	//Вместимость
-	cap int
-	l *list.List
-	m map[int]int
+	cap     int
+	l       *list.List
+	m       map[int]int
 	listMap map[int]*list.Element
 }
 
-func (cache LRUCache) Get(key int) (v int, ok bool){
+func (cache LRUCache) Get(key int) (v int, ok bool) {
 	v, ok = cache.m[key]
 
 	if ok {
 		cache.moveKeyToBack(key)
 	}
 
-	return 
+	return
 }
 
 func (cache LRUCache) Set(key, value int) {
-	_, ok := cache.m[key]
+	if cache.cap > 0 {
+		_, ok := cache.m[key]
 
-	if !ok && len(cache.m) == cache.cap {
-		cache.removeOldKey()
+		if !ok && len(cache.m) == cache.cap {
+			cache.removeOldKey()
+		}
+
+		cache.m[key] = value
+		cache.moveKeyToBack(key)
 	}
-
-	cache.m[key] = value
-	cache.moveKeyToBack(key)
-
-	return 
 }
 
 func (cache LRUCache) Clear() {
@@ -48,21 +48,25 @@ func (cache LRUCache) Clear() {
 }
 
 func (cache LRUCache) Range(f func(key, value int) bool) {
-	for k, v := range cache.m {
-		cache.moveKeyToBack(k)
+	elem := cache.l.Front()
 
-		check := f(k, v)
+	for elem != nil {
+		k := elem.Value.(int)
 
-		if !check {
-			return 
+		v := cache.m[k]
+
+		if !f(k, v) {
+			return
 		}
+
+		elem = elem.Next()
 	}
 }
 
 func (cache LRUCache) removeOldKey() {
 	first := cache.l.Front()
 
-	firstKey := first.Value
+	firstKey := first.Value.(int)
 
 	delete(cache.m, firstKey)
 	delete(cache.listMap, firstKey)
@@ -76,16 +80,14 @@ func (cache LRUCache) moveKeyToBack(key int) {
 		cache.l.Remove(v)
 	}
 
-	cache.listMap[key] = cache.l.PushBack(key);
-
-	return 
+	cache.listMap[key] = cache.l.PushBack(key)
 }
 
 func New(cap int) Cache {
 	return LRUCache{
-		cap: cap,
-		l: list.New(),
-		m: make(map[int]int, cap),
+		cap:     cap,
+		l:       list.New(),
+		m:       make(map[int]int, cap),
 		listMap: make(map[int]*list.Element, cap),
 	}
 }
