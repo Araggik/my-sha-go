e7f7c46262fa22f3b304c4d4e9dac92e3701908b
Vlad Matveev <vladislaw0159@gmail.com>
2025-08-30 19:35:45 +0500

    Сделал yamlembed

--- yamlembed/types.go
+++ yamlembed/types.go
@@ -1,19 +1,57 @@
 package yamlembed
 
+import "strings"
+
 type Foo struct {
-	A string
-	p int64
+	A string `yaml:"aa"`
+	p int64  `yaml:"p"`
 }
 
 type Bar struct {
-	I      int64
-	B      string
-	UpperB string
-	OI     []string
-	F      []any
+	I      int64    `yaml:",omitempty"`
+	B      string   `yaml:"b"`
+	UpperB string   `yaml:"-"`
+	OI     []string `yaml:",omitempty"`
+	F      []any    `yaml:"f,flow"`
+}
+
+func (b *Bar) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var err error
+
+	originMap := make(map[string]any)
+
+	err = unmarshal(originMap)
+
+	b.B = originMap["b"].(string)
+	b.UpperB = strings.ToUpper(b.B)
+
+	oi := originMap["oi"].([]any)
+	for _, v := range oi {
+		b.OI = append(b.OI, v.(string))
+	}
+
+	b.F = originMap["f"].([]any)
+
+	return err
 }
 
 type Baz struct {
 	Foo `yaml:",inline"`
 	Bar `yaml:",inline"`
+}
+
+func (b *Baz) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var err error
+
+	originMap := make(map[string]any)
+
+	err = unmarshal(&originMap)
+
+	if err == nil {
+		b.Foo.A = originMap["aa"].(string)
+
+		err = b.Bar.UnmarshalYAML(unmarshal)
+	}
+
+	return err
 }
