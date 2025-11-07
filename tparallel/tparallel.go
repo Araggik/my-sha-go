466e2061534a149fd1bf0a12bf0b04f97d56efa7
Матвеев Владислав Анатольевич <matveev.va@corp.ertelecom.loc>
2025-11-07 09:31:51 +0500

    Проверить tparallel

--- tparallel/tparallel.go
+++ tparallel/tparallel.go
@@ -3,16 +3,49 @@
 package tparallel
 
 type T struct {
+	parallelCount int
+	seqCh chan struct{}
+	parallelCh chan struct{}
 }
 
 func (t *T) Parallel() {
-	panic("implement me")
+	t.parallelCount++
+	t.seqCh  <- struct{}{}
+	<- t.parallelCh
 }
 
 func (t *T) Run(subtest func(t *T)) {
-	panic("implement me")
+	pCount := t.parallelCount
+
+	for _, f := subtest {
+		go func() {
+			defer func() {
+				t.seqCh  <- struct{}{}
+			}()
+			f(t)
+		}()
+
+		<- t.seqCh
+	}
+
+	parallelSubCount := t.parallelCount - pCount
+
+	if parallelSubCount > 0 {
+		for i := range parallelSubCount {
+			t.parallelCh <- struct{}{}
+		}
+
+		for t.parallelCount != pCount {
+			<- t.seqCh
+			t.parallelCount--
+		}
+	}
 }
 
 func Run(topTests []func(t *T)) {
-	panic("implement me")
+	t := &T{
+		seqCh: make(chan struct{})
+	}
+
+	t.Run(topTests)
 }
