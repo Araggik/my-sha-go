739a49605488b6e69c54f2e1a9356dcd6be56e33
Матвеев Владислав Анатольевич <matveev.va@corp.ertelecom.loc>
2025-12-03 11:30:02 +0500

    Доделать keylock

--- keylock/keylock.go
+++ keylock/keylock.go
@@ -2,12 +2,45 @@
 
 package keylock
 
-type KeyLock struct{}
+import (
+	"sort"
+	"strings"
+)
+
+type KeyLock struct {
+	//Ключ в map - конкатинация необходимых ключей для лока
+	chMap map[string][](chan struct{})
+	//Мютекс для работы с chMap
+	mapCh chan struct{}
+}
 
 func New() *KeyLock {
-	panic("implement me")
+	return &KeyLock{
+		chMap: make(map[string][](chan struct{})),
+		mapCh: make(chan struct{}, 1),
+	}
 }
 
 func (l *KeyLock) LockKeys(keys []string, cancel <-chan struct{}) (canceled bool, unlock func()) {
-	panic("implement me")
+	sort.Strings(keys)
+
+	mapKey := strings.Join(keys, " ")
+
+	mapCh <- struct{}{}
+
+	waitCh := l.receiveWaitCh(mapKey)
+
+	<-mapCh
+}
+
+func (l *KeyLock) receiveWaitCh(mapKey string) chan struct{} {
+	chArr, ok := l.chMap[mapKey]
+
+	if !ok {
+		chArr = make([](chan struct{}))
+	}
+
+	//TODO: создание канала для LockKeys и добавление его в map, если он нужен
+
+	l.chMap[mapKey] = chArr
 }
